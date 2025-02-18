afb4d5267bed4954b29b1e84dc65111079f3e0f6
pejorasan <petteri.rasanen2@gmail.com>
2025-02-19 00:00:33 +0900

    Implement data save load system using player prefs

---
+++ Assets/Scripts/SaveDataManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SaveDataManager : MonoBehaviour
+{
+    public static SaveDataManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SaveProgress(string key, HashSet<string> correctAnswers)
+    {
+        string savedData = string.Join(",", correctAnswers.ToArray());
+        PlayerPrefs.SetString(key, savedData);
+        PlayerPrefs.Save();
+        Debug.Log($"Saved data for {key}: {savedData}");
+    }
+
+    public HashSet<string> LoadProgress(string key)
+    {
+        string data = PlayerPrefs.GetString(key, "");
+        Debug.Log($"Loaded data for {key}: '{data}'");
+
+        // Make sure empty strings do not contribute to the HashSet count
+        if (string.IsNullOrEmpty(data))
+        {
+            return new HashSet<string>(); // Return an empty set properly
+        }
+
+        return new HashSet<string>(data.Split(',').Where(s => !string.IsNullOrEmpty(s)));
+    }
+
+    public void ResetProgress(string key)
+    {
+        PlayerPrefs.DeleteKey(key);
+        PlayerPrefs.Save();
+        Debug.Log($"Reset data for {key}");
+
+        // Verify that data is cleared
+        string data = PlayerPrefs.GetString(key, "");
+        Debug.Log($"Data after reset for {key}: {data}");
+    }
+}
